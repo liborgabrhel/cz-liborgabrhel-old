203d673c6dbbdfa4c989ff796607b147b2195a18
liborgabrhel <gabrhel.libor@gmail.com>
2019-03-03 19:46:40 +0100

    Added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
     `gatsby-plugin-typescript`,
     `gatsby-plugin-tslint`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
